c4669ec057c6a587f819d432a724c4da4d44e24b
Andrew Janke <janke@pobox.com>
2015-02-27 23:11:00 -0500

    Add Cygwin support to installer. * Balk at incompatible Windows/MSYS git * Test for chsh presence before trying to use it * Replace non-portable `[[ ... ]]` and `[ x = *pattern* ]` constructs

--- tools/install.sh
+++ tools/install.sh
@@ -44,9 +44,21 @@ fi
 umask g-w,o-w
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
-  printf "git not installed\n"
-  exit
+hash git >/dev/null 2>&1 || {
+  echo "Error: git is not installed"
+  exit 1
+}
+# The Windows (MSYS) Git is not compatible with normal use on cygwin
+if [ "$OSTYPE" = cygwin ]; then
+  if git --version | grep msysgit > /dev/null; then
+    echo "Error: Windows/MSYS Git is not supported on Cygwin"
+    echo "Error: Make sure the Cygwin git package is installed and is first on the path"
+    exit 1
+  fi
+fi
+env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+  echo "Error: git clone of oh-my-zsh repo failed"
+  exit 1
 }
 
 printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
@@ -68,15 +80,14 @@ export PATH=\"$PATH\"
 " ~/.zshrc > ~/.zshrc-omztemp
 mv -f ~/.zshrc-omztemp ~/.zshrc
 
-# If this user's login shell is not already "zsh", attempt to switch.
-if [ "$(expr "$SHELL" : '.*/\(.*\)')" != "zsh" ]; then
-  # If this platform provides a "chsh" command (not Cygwin), do it, man!
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
   if hash chsh >/dev/null 2>&1; then
     echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
     chsh -s $(grep /zsh$ /etc/shells | tail -1)
-  # Else, suggest the user do so manually.
   else
-    echo "\033[0;34mPlease manually change your default shell to zsh!\033[0m"
+    echo "I can't change your shell automatically because this system does not have chsh."
+    echo "Please edit /etc/passwd to set your default shell to zsh."
   fi
 fi
 
